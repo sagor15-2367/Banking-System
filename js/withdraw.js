9f92584cb3682951b90710784603ec08be15b2bb
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-29 14:03:32 +0600

    withdraw done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,43 @@
-console.log('sagor')
+// step:1
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+
+    // step:2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step:7
+    withdrawField.value = '';
+
+    // conditions
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+    // step:3
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    // step:5
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // conditions
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('No balance');
+        return;
+    }
+
+    // step:4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step:6
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+
+})
